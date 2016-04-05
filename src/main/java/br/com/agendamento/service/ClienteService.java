2297dcf049c34427efcc4e59bbbbde48cfc83340
Jonas <jonas.costa@icloud.com>
2016-04-05 19:47:39 -0300

    metodos editar e excluir

--- src/main/java/br/com/agendamento/service/ClienteService.java
+++ src/main/java/br/com/agendamento/service/ClienteService.java
@@ -2,13 +2,14 @@ package br.com.agendamento.service;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import br.com.agendamento.DAO.ClienteDAO;
 import br.com.agendamento.domain.Cliente;
@@ -59,6 +60,42 @@ public class ClienteService {
 
 			return clienteJson;
 
+		}
+		
+		
+		// http://localhost:8081/Agendamento/service/clientes/codigo
+		@DELETE
+		@Path("{id}")
+		public String excluir(@PathParam("id") Long id) {
+			
+			ClienteDAO clienteDAO = new ClienteDAO();
+			Cliente cliente = clienteDAO.buscarPorcodigo(id);
+			
+			clienteDAO.excluir(cliente);
+			
+			Gson gson = new Gson();
+			String clienteJson = gson.toJson(cliente);
+			
+			return clienteJson;
+			
+		}
+		
+		
+		// http://localhost:8081/Agendamento/service/clientes
+		@PUT
+		public String editar(String json) {
+			
+			Gson gson = new Gson();
+			Cliente cliente = gson.fromJson(json, Cliente.class);
+			
+			ClienteDAO clienteDAO = new ClienteDAO();
+			clienteDAO.editar(cliente);
+			
+			String clienteJson = gson.toJson(cliente);
+			
+			return clienteJson;
+			
+			
 		}
 
 }
