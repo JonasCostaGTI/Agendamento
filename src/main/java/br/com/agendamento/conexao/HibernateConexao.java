aaf270dc4cf16c7e39f84422f4466eef945a3838
Jonas <jonas.costa@icloud.com>
2016-07-05 20:48:08 -0300

    commit 2

--- src/main/java/br/com/agendamento/conexao/HibernateConexao.java
+++ src/main/java/br/com/agendamento/conexao/HibernateConexao.java
@@ -20,10 +20,10 @@ public class HibernateConexao {
 			// busca o arquico hibernate.cfg.xml
 			Configuration configuracao = new Configuration().configure();
 			
-//			URL url = new URL("jdbc:mysql://"+System.getenv("OPENSHIFT_MYSQL_DB_HOST") + ":" + 
-//					System.getenv("OPENSHIFT_MYSQL_DB_PORT")+ "/agendahorario");
-//			
-//			configuracao.configure(url);
+			URL url = new URL("jdbc:mysql://"+System.getenv("OPENSHIFT_MYSQL_DB_HOST") + ":" + 
+					System.getenv("OPENSHIFT_MYSQL_DB_PORT")+ "/agendahorario");
+			
+			configuracao.configure(url);
 
 			ServiceRegistry registro = new StandardServiceRegistryBuilder().applySettings(configuracao.getProperties())
 					.build();
